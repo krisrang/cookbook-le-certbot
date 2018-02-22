32a539a6ac69c77e1a002788d71ee50e1260248d
Kristjan Rang <mail@rang.ee>
2018-02-22 15:22:05 +0200

    Support certbot install on debian

--- resources/certbot.rb
+++ resources/certbot.rb
@@ -1,21 +1,30 @@
 resource_name :certbot
 
 action :install do
-  apt_repository 'certbot' do
-    uri          'ppa:certbot/certbot'
-    distribution node['lsb']['codename']
-  end
+  if platform?('ubuntu')
+    apt_repository 'certbot' do
+      uri          'ppa:certbot/certbot'
+      distribution node['lsb']['codename']
+    end
 
-  package 'certbot' do
-    action :install
+    package 'certbot' do
+      action :install
+    end
+  elsif platform?('debian')
+    package 'certbot' do
+      default_release "#{node['lsb']['codename']}-backports"
+      action :install
+    end
   end
 end
 
 action :remove do
-  apt_repository 'certbot' do
-    uri          'ppa:certbot/certbot'
-    distribution node['lsb']['codename']
-    action :remove
+  if platform?('ubuntu')
+    apt_repository 'certbot' do
+      uri          'ppa:certbot/certbot'
+      distribution node['lsb']['codename']
+      action :remove
+    end
   end
 
   package 'certbot' do
